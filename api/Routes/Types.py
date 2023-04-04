7a4d1530ed1120bbf2ab7124c4b459f7745d16f7
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-04-04 19:32:51 -0300

    style

--- api/Routes/Types.py
+++ api/Routes/Types.py
@@ -3,11 +3,13 @@ from flask_restful import Resource
 from flask import request
 from models.Types_model import TypesModel
 
+
 class Types(Resource):
     def get(self):
         types = TypesModel.find_all_types()
         return types
-    
+
+
 class Type(Resource):
     def get(self, id):
         type = TypesModel.find_type(self, id)
@@ -18,14 +20,12 @@ class Type(Resource):
         type = TypesModel(dados)
         TypesModel.save_type(type)
         return {"message": "the type has been created"}, 201
-    
+
     def delete(self, id):
         TypesModel.delete_type(self, id)
         return {"message": "the type has been deleted"}, 202
-    
+
     def put(self, id):
         dados = request.get_json()
         message = TypesModel.update_type(self, id, dados)
         return message
-    
-
