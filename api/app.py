5c2a9d8545f3c1e90d844719606bcb35ffc790b1
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-02-18 23:54:22 -0300

    adiciona dockerfile e instruções de instalação

--- api/app.py
+++ api/app.py
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     from sql_alchemy import banco
 
     banco.init_app(app)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
