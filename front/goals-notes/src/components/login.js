5336556b94b5e7dff399f84cadc21f7c54a1f22b
Caio Milani <caiocmilani@gmail.com>
2024-02-26 21:04:13 -0300

    Subindo a tela de login novinha e a cap

--- front/goals-notes/src/components/login.js
+++ front/goals-notes/src/components/login.js
@@ -1,66 +1,112 @@
-import React from 'react';
-import { useState } from 'react'
-import { login } from '../services/api_requests'
+import React, { useEffect, useState } from 'react';
+import { login } from '../services/api_requests';
 
-function LoginComponent(){
-    const[surname, setsurname] = useState()
-    const[password, setPassword] = useState()
-    const[message,setMessage] = useState('')
+function LoginComponent() {
+    const [surname, setsurname] = useState();
+    const [password, setPassword] = useState();
+    const [message, setMessage] = useState('');
+    const [primeiraVez, setPrimeiraVez] = useState(true);
 
     const surnameChange = (e) => {
-        setsurname(e.target.value)
-    }
+        setsurname(e.target.value);
+    };
+
     const passwordChange = (e) => {
-        setPassword(e.target.value)
-    }
+        setPassword(e.target.value);
+    };
+
+    const capivaraOlhos = (focus) => {
+        const olhos = document.getElementById("cap");
+        if (focus) {
+            olhos.classList.add("fecha");
+        } else {
+            olhos.classList.remove("fecha");
+        }
+    };
+
     async function login_user(event) {
         event.preventDefault();
         const user_json = {
-          "surname": surname,
-          "password": password
-        }
-        const response = await login(user_json)
-        console.log(response)
+            "surname": surname,
+            "password": password
+        };
+        const response = await login(user_json);
+        console.log(response);
         if (response.message === "user is already logged in") {
-          setMessage("usuário já se encontra logado")
-        } else if(response.message === "login not found: wrong password or surname" || response === false ){
-          setMessage("login Incorreto, por favor, verifique sua senha ou seu nickname")
-        } else if(response.message === "user logged in successfully"){
-            setMessage("logado com sucesso") //adicionar aqui um redirect para a página home
-        }else{
-            setMessage("erro inexperado")
+            setMessage("usuário já se encontra logado");
+        } else if (response.message === "login not found: wrong password or surname" || response === false) {
+            setPrimeiraVez(false);
+            setMessage("login Incorreto, por favor, verifique sua senha ou seu nickname");
+        } else if (response.message === "user logged in successfully") {
+            setMessage("logado com sucesso"); //adicionar aqui um redirect para a página home
+        } else {
+            setMessage("erro inexperado");
         }
-      }
+    }
 
-    return(
-        <>
-            <form onSubmit={login_user}>
-                <div>
-                    <h3>Nome:</h3>
-                    <input 
-                        type="text"
-                        required="required"
-                        onChange={surnameChange}
-                        id="surname"
-                        placeholder="informe o seu nome"
-                    />
-                </div>
-                <div>
-                    <h3>Password:</h3>
-                    <input 
-                        type="password"
-                        required="required"
-                        onChange={passwordChange}
-                        id="password"
-                        placeholder="informe a sua senha"
-                    />
-                </div>
-                <input type = "submit" value = "entar"/>
-                <input type = "submit" value = "entar"/>
-                {message ? <h3>{message}</h3> : ''}
-            </form>
-        </>
+    return (
+        <div>
+            {primeiraVez && (
+                <form className='LoginForm' onSubmit={login_user}>
+                    <h1 className='textos'>Faça o Login em sua Conta</h1>
+                    <div className='registroLogin'>
+                        <input
+                            className='textos'
+                            type="text"
+                            required="required"
+                            onChange={surnameChange}
+                            id="surname"
+                            placeholder="Informe o E-mail ou nome de usuário"
+                        />
+                    </div>
+                    <div className='registroSenha'>
+                        <input
+                            className='textos'
+                            type="password"
+                            required="required"
+                            onChange={passwordChange}
+                            onFocus={() => capivaraOlhos(true)}
+                            onBlur={() => capivaraOlhos(false)}
+                            id="password"
+                            placeholder="informe a sua senha"
+                        />
+                        <a className='textos esqueciSenha'>Esqueceu a senha?</a>
+                    </div>
+                    <button className='textos botaoLogar' type="submit" value="entar">Entrar</button>
+                </form>
+            )}
+            {!primeiraVez && (
+                <form className='LoginForm' onSubmit={login_user}>
+                    <h1 className='textos'>Faça o Login em sua Conta</h1>
+                    <div className='registroLogin'>
+                        <input
+                            className='textos'
+                            type="text"
+                            required="required"
+                            onChange={surnameChange}
+                            id="surname"
+                            placeholder="Informe o E-mail ou nome de usuário"
+                        />
+                    </div>
+                    <div className='registroSenha'>
+                        <input
+                            className='textos'
+                            type="password"
+                            required="required"
+                            onChange={passwordChange}
+                            onFocus={() => capivaraOlhos(true)}
+                            onBlur={() => capivaraOlhos(false)}
+                            id="password"
+                            placeholder="informe a sua senha"
+                        />
+                        <a className='textos esqueciSenha'>Esqueceu a senha?</a>
+                    </div>
+                    <button className='textos botaoLogar' type="submit" value="entar">Entrar</button>
+                    {message ? <div className='textos alertaLogin'>{message}</div> : ''}
+                </form>
+            )}
+        </div>
     )
 }
 
-export default LoginComponent
+export default LoginComponent;
--- front/goals-notes/src/pages/initial_page/initial_page.js
+++ front/goals-notes/src/pages/initial_page/initial_page.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import LoginSlider from '../../components/loginSlider'
+import LoginComponent from '../../components/login.js';
+import RegisterComponent from '../../components/register.js';
+import Container from 'react-bootstrap/Container';
+import '../../components/Style/loginStyle.css'
+
 const Initial = () => {
   return (
-    <div>
-      <LoginSlider/>
-      <br></br>
-    </div>
+    <section className='sectionLoginForm'>
+      <img className='logoCapivara capivaraOlha' src='/content/capivaraSemOlhos.png' alt="capivaraSemOlhos" />
+      <div id='cap' className="eyes">
+          <div className="eye" id="leftEye"></div>
+      </div>
+      <div className='cardLogin grid'>
+          <LoginComponent />
+      </div>
+      <div className='cardRegister grid'>
+          <RegisterComponent />
+      </div>
+    </section>
   );
 }
 
