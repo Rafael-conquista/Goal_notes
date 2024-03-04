1ca33de52426d59b2271297a78595d30484bda13
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-03-04 18:56:08 -0300

    adiciona controllers para a rota de posts

--- api/Routes/Posts.py
+++ api/Routes/Posts.py
@@ -1,22 +1,25 @@
 from sql_alchemy import banco
 from flask_restful import Resource
 from flask import request
-from utils import main_queries
+from controllers.Posts_controller import PostsController
 
 
 class Posts(Resource):
     def get(self):
-        return {"msg": "ta funcionando"}
+        return PostsController.find_all_posts()
 
 
 class Post(Resource):
     def get(self, id):
-        return {"msg": "ta funcionando"}
+        return PostsController.find_post(id)
 
     def put(self, id):
-        return {"msg": "ta funcionando"}
+        dados = request.get_json()
+        return PostsController.update_post(id, dados)
 
 class PostCreator(Resource):
-    def post():
-        return {"msg": "ta funcionando"}
+    def post(self):
+        dados = request.get_json()
+        user_id = dados['id_user']
+        return PostsController.make_post(dados, user_id)
 
--- api/models/Posts_model.py
+++ api/models/Posts_model.py
@@ -2,18 +2,19 @@ from sql_alchemy import banco
 from sqlalchemy import ForeignKey
 
 
-class TypesModel(banco.Model):
-    __tablename__ = "Types"
+class PostsModel(banco.Model):
+    __tablename__ = "Posts"
 
     id = banco.Column(banco.Integer, primary_key=True, autoincrement=True)
     numLikes = banco.Column(banco.Integer, default=0)
-    desc = banco.Column(banco.String(200))
-    id_user = banco.Column(banco.Integer, ForeignKey("Users.id"))
-    id_goal = banco.Column(banco.Intefer, ForeignKey("Goals.goals_id"))
+    desc = banco.Column(banco.String(200), nullable=False)
+    id_user = banco.Column(banco.Integer, ForeignKey("Users.id"), nullable=False)
+    id_goal = banco.Column(banco.Integer, ForeignKey("Goals.goals_id"), nullable=False)
     dataCadastro = banco.Column(banco.DateTime, default=banco.func.now())
     dataAlteracao = banco.Column(banco.DateTime, default=banco.func.now())
     excluido = banco.Column(banco.Boolean, default=False)
 
     def __init__(self, dados):
-        self.name = dados.get("name", None)
-        self.obs = dados.get("obs", None)
+        self.desc = dados.get("desc", None)
+        self.id_user = dados.get("id_user", None)
+        self.id_goal = dados.get("id_goal", None)
