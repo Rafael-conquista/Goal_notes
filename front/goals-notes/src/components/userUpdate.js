820e111aeaa928e3b86e005fed382a580d193cc1
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-04-08 19:24:22 -0300

    Adiciona estilização básica para a página de alteração de usuário

--- front/goals-notes/src/components/userUpdate.js
+++ front/goals-notes/src/components/userUpdate.js
@@ -1,37 +1,27 @@
 import './Style/userStyle.css'
+import { FaAngleLeft } from "react-icons/fa6";
 import cap_default from '../images/cap_default.jpg';
 
 function UserUpdateComponent(){
-    //preciso adicionar uma estilização minimamente decente para a tela
     //preciso adicionar a lógica e as requisições para realizar a alteração de usuario
     return(
         <div className="user_container">
-            <div>
-                <h3>Voltar para tela anterior(botão)</h3>
+            <div className='return_button'>
+                <FaAngleLeft/>
             </div>
-            <div>
-                <h3>Editar</h3>
-            </div>
-            <div>
+            <div className='info_input'>
                 <img src={cap_default} alt='vazio' className="cap_welcome_page" />
-                <input type='text' placeholder='seu apelido'/>
-            </div>
-            <div>
-                <input type='text' placeholder='seu email'/>
-            </div>
-            <div>
-                <input type="password" placeholder='password' required="required"/>
-                <input type="password" placeholder='confirm password' required="required"/>
+                <input type='text' placeholder='seu apelido' className='nickname_input'/>
+                <input type='text' placeholder='seu email' className='email_input'/>
+                <input type="password" placeholder='password' required="required" className='password_input'/>
+                <input type="password" placeholder='confirm password' required="required" className='password_input'/>
             </div>
             <div className='submit_buttons'>
-                <span>
-                    Descartar
-                </span>
-                <div className='submit_comment_button'>
-                    Publicar
-                </div>
                 <div className='submit_comment_button'>
                     Excluir Perfil
+                </div>
+                <div className='submit_comment_button'>
+                    Publicar
                 </div>
             </div>
         </div>
