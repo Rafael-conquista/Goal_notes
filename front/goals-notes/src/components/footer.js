855895220023aa98df11d8e39ef3e8dee3bfa8a5
ThiagoD22 <thiago50.dants@gmail.com>
2024-05-09 20:52:44 -0300

    criacao footer

---
+++ front/goals-notes/src/components/footer.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import './Style/footer.css';
+
+function Footer() {
+    useEffect(() => {
+        // Seu código JavaScript para o footer, se necessário
+    }, []);
+
+    return (
+        <footer id="Footer" className="Footer">
+            <ul className="social_list">
+                <li className="social_list li">
+                    <FaFacebook />
+                </li>
+                <li className="social_list li">
+                    <FaInstagram />
+                </li>
+            </ul>
+            <p className="copy_right">&copy; 2024. Todos os direitos reservados a <span>Capy ❤</span></p>
+        </footer>
+    );
+}
+
+export default Footer;
