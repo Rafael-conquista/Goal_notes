c63359b7ef745ad02256ce35f2f0df3f922ce0d3
Caio Milani <caiocmilani@gmail.com>
2024-07-11 23:14:54 -0300

    Atualizando a model store

--- api/models/Store_model.py
+++ api/models/Store_model.py
@@ -13,11 +13,13 @@ class StoreModel(banco.Model):
     dataCadastro = banco.Column(banco.DateTime, default=banco.func.now())
     dataAlteracao = banco.Column(banco.DateTime, onupdate=banco.func.now())
     active = banco.Column(banco.Boolean, default=False) 
-    enum = banco.Column(banco.Integer, default=None)
-    type = banco.Column(banco.Integer, default=None)
+    enum = banco.Column(banco.Integer)
+    type = banco.Column(banco.Integer)
 
     def __init__(self, dados):
         self.price = dados.get("price", None)
         self.initial_data = dados.get("initial_data", None)
         self.end_date = dados.get("end_date", None)
         self.active = dados.get("active", None)
+        self.enum = dados.get("enum", None)
+        self.type = dados.get("type", None)
