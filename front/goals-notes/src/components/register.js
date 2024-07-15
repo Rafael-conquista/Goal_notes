e1c379432b3181ae7f6f0ce52d9663198f8e2ea6
ErickStoppa <erick.s.s.martins31@gmail.com>
2024-07-15 15:59:54 -0300

    fix: responsividade e alguns desing arrumados

--- front/goals-notes/src/components/register.js
+++ front/goals-notes/src/components/register.js
@@ -1,213 +1,198 @@
-import React, { useState, useEffect } from 'react';
-import { token_storage } from '../utils/token_verify';
-import { register } from '../services/api_requests'
-import Loading from './loading.js';
+import React, { useState, useEffect } from "react";
+import { token_storage } from "../utils/token_verify";
+import { register } from "../services/api_requests";
+import Loading from "./loading.js";
 
-function RegisterComponent(){
-    const [email, setEmail] = useState()
-    const [age, setAge] = useState()
-    const [surname, setSurname] = useState()
-    const [password, setPassword] = useState()
-    const [confirm, setConfirm] = useState()
-    const [message, setMessage] = useState('')
-    const [loading, setloading] = useState(false);
-    const [primeiraVez, setPrimeiraVez] = useState(true);
-    const [sucesso, setSucesso] = useState(false);
+function RegisterComponent() {
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const [surname, setSurname] = useState();
+  const [password, setPassword] = useState();
+  const [confirm, setConfirm] = useState();
+  const [message, setMessage] = useState("");
+  const [loading, setloading] = useState(false);
+  const [primeiraVez, setPrimeiraVez] = useState(true);
+  const [sucesso, setSucesso] = useState(false);
 
-    const emailChange = (e) => {
-      setEmail(e.target.value)
-      console.log(e.target.value)
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+  const surnameChange = (e) => {
+    setSurname(e.target.value);
+  };
+  const passwordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const confirmChange = (e) => {
+    setConfirm(e.target.value);
+  };
+  const ageChange = (e) => {
+    setAge(e.target.value);
+  };
+
+  async function register_user(event) {
+    setloading(true);
+    event.preventDefault();
+    if (password !== confirm) {
+      setMessage("Sua senha e sua confirmação de senha não coincidem");
+      setPrimeiraVez(false);
+      setloading(false);
+      return;
     }
-    const surnameChange = (e) => {
-      setSurname(e.target.value)
-    }
-    const passwordChange = (e) => {
-      setPassword(e.target.value)
-    }
-    const confirmChange = (e) => {
-      setConfirm(e.target.value)
-    }
-    const ageChange = (e) => {
-      setAge(e.target.value)
-    }
-    
-    async function register_user(event) {
-      setloading(true);
-      event.preventDefault();
-      if (password !== confirm) {
-        setMessage('Sua senha e sua confirmação de senha não coincidem')
-        setPrimeiraVez(false);
-        setloading(false);
-        return
-      }
-      const user_json = {
-        "email": email,
-        "name": surname,
-        "age": age,
-        "surname": surname,
-        "password": password
-      }
-      const response = await register(user_json)
-      console.log(response)
-      if (response.message === "the user has been created") {
-        setMessage("usuário criado com sucesso")
-        setSucesso(true);
-        setPrimeiraVez(false);
-        setloading(false);
-        token_storage(response.token)
-        
-        sessionStorage.setItem('first_acess', true)
-        window.location.href = '/capCreate'
-      } else {
-        setMessage("Ocorreu um erro durante a criação do usuário")
-        setPrimeiraVez(false);
-        setloading(false);
-      }
-    }
-     
-    const capivaraOlhos = (focus) => {
-      const olhos = document.getElementById("cap");
-      if (focus) {
-          olhos.classList.add("fecha");
-      } else {
-          olhos.classList.remove("fecha");
-      }
+    const user_json = {
+      email: email,
+      name: surname,
+      age: age,
+      surname: surname,
+      password: password,
     };
-    document.addEventListener("mousemove", function(e) {
-      const eyes = document.querySelectorAll(".eye");
-      eyes.forEach(eye => {
-          const bounds = eye.getBoundingClientRect();
-          const x = bounds.left + bounds.width / 2;
-          const y = bounds.top + bounds.height / 2;
-          const radianAngle = Math.atan2(e.clientY - y, e.clientX - x);
-          const angle = radianAngle * (180 / Math.PI);
-          const distance = Math.min(Math.hypot(e.clientX - x, e.clientY - y), 12);
-          eye.style.transform = `translate(-50%, -50%) rotate(${angle}deg) translate(${distance}px)`;
-        });
-      });
+    const response = await register(user_json);
+    console.log(response);
+    if (response.message === "the user has been created") {
+      setMessage("usuário criado com sucesso");
+      setSucesso(true);
+      setPrimeiraVez(false);
+      setloading(false);
+      token_storage(response.token);
 
-      const [telaMaiorCelular, setTelaMaiorCelular] = useState(window.innerWidth > 1000);
-
-      useEffect(() => {
-        const verificarTamanhoDaTela = () => {
-          setTelaMaiorCelular(window.innerWidth > 1000);
-        };
-
-        window.addEventListener('resize', verificarTamanhoDaTela);
-
-        return () => {
-          window.removeEventListener('resize', verificarTamanhoDaTela);
-        };
-      }, []);
-
-    const primeiraVezAtualizar = (event) => {
-      setPrimeiraVez(true);
+      sessionStorage.setItem("first_acess", true);
+      window.location.href = "/capCreate";
+    } else {
+      setMessage("Ocorreu um erro durante a criação do usuário");
+      setPrimeiraVez(false);
+      setloading(false);
+    }
   }
-  
-    return(
+
+  const capivaraOlhos = (focus) => {
+    const olhos = document.getElementById("cap");
+    if (focus) {
+      olhos.classList.add("fecha");
+    } else {
+      olhos.classList.remove("fecha");
+    }
+  };
+  document.addEventListener("mousemove", function (e) {
+    const eyes = document.querySelectorAll(".eye");
+    eyes.forEach((eye) => {
+      const bounds = eye.getBoundingClientRect();
+      const x = bounds.left + bounds.width / 2;
+      const y = bounds.top + bounds.height / 2;
+      const radianAngle = Math.atan2(e.clientY - y, e.clientX - x);
+      const angle = radianAngle * (180 / Math.PI);
+      const distance = Math.min(Math.hypot(e.clientX - x, e.clientY - y), 12);
+      eye.style.transform = `translate(-50%, -50%) rotate(${angle}deg) translate(${distance}px)`;
+    });
+  });
+
+  const primeiraVezAtualizar = (event) => {
+    setPrimeiraVez(true);
+  };
+
+  return (
+    <>
+      {loading && <Loading />}
       <div>
-        {loading && (
-          <Loading/> 
-        )}
-        <form className='registroUsuario' onSubmit={register_user}>
-          <div>
-            <h1 className='textos'>Novo Aqui?</h1>
-            <h2 className='textos'>Cadastre-se agora para poder organizar sua rotina e ter uma amiga "capivarinha" para te acompanhar nessa jornada!</h2>
-          </div>
-          <div className='formRegistroUsuario'>
-              <input
-                type="email"
-                required="required"
-                onChange={emailChange}
-                id="email"
-                placeholder='Informe o E-mail'
-              />
-              {!telaMaiorCelular &&(
-                <input className='textos'
-                type="text"
-                required="required"
-                onChange={surnameChange}
-                id="surname"
-                placeholder='Nome do Usuário'
-              />
-              )}
-              {telaMaiorCelular &&(
-                <input className='textos'
-                  type="text"
-                  required="required"
-                  onChange={surnameChange}
-                  id="surname"
-                  placeholder='Informe o seu nome de usuário'
-                />
-              )}
-              <input className='textos'
-                type="date"
-                required="required"
-                onChange={ageChange}
-                id="age"
-                placeholder='data de nascimento'
-              />
-          </div>
-          <div className='formRegistroUsuario'>
-            {!telaMaiorCelular &&(
-              <>
-                <input className='textos'
-                  type="password"
-                  required="required"
-                  onChange={passwordChange}
-                  id="password"
-                  placeholder='Insira sua Senha'
-                />
-                <input className='textos'
-                  type="password"
-                  id="confirm_password"
-                  required="required"
-                  onChange={confirmChange}
-                  placeholder="Confirme a Senha"
-                />
-              </>
-            )}
-            {telaMaiorCelular &&(
-              <>
-                <input className='textos'
-                  type="password"
-                  required="required"
-                  onChange={passwordChange}
-                  onFocus={() => capivaraOlhos(true)}
-                  onBlur={() => capivaraOlhos(false)}
-                  id="password"
-                  placeholder='Insira sua Senha'
-                />
-                <input className='textos'
-                  type="password"
-                  id="confirm_password"
-                  required="required"
-                  onChange={confirmChange}
-                  onFocus={() => capivaraOlhos(true)}
-                  onBlur={() => capivaraOlhos(false)}
-                  placeholder="confirme a Senha"
-                />
-              </>
-            )}
-          </div>
-          {telaMaiorCelular &&(
-              <button className='textos botaoRegistrar' type="submit" value="entar">Entrar</button>
-          )}
-          {!telaMaiorCelular &&(
-              <div className='coletaneaBotoesLogar'>
-                  <button className='textos botaoRegistrar' type="submit" value="entar">Registrar-se</button>
-                  <a className='textos botaoRegistrar botaoUsuarUsuario' onClick={() => primeiraVezAtualizar(true)} href="#login">Logar Usuário</a>
-              </div>
-          )}
-          {!primeiraVez && !sucesso && (
-            message ? <div className='alertaRegistro'>{message}</div> : ''
-          )}
-          {!primeiraVez && sucesso && (
-            message ? <div className='alertaRegistroSucesso'>{message}</div> : ''
-          )}
-        </form>
+        <h1 className="textos">Novo Aqui?</h1>
+        <h2 className="textos">
+          Cadastre-se agora para poder organizar sua rotina e ter uma amiga
+          "capivarinha" para te acompanhar nessa jornada!
+        </h2>
       </div>
-    )
+      <form className="formRegistroUsuario" onSubmit={register_user}>
+        <input
+          type="email"
+          required="required"
+          onChange={emailChange}
+          id="email"
+          placeholder="E-mail"
+        />
+
+        <input
+          className="textos"
+          type="text"
+          required="required"
+          onChange={surnameChange}
+          id="surname"
+          placeholder="Informe o usuário"
+        />
+
+        <input
+          className="textos"
+          type="date"
+          required="required"
+          onChange={ageChange}
+          id="age"
+          placeholder="Data de nascimento"
+        />
+
+        <input
+          className="textos"
+          type="password"
+          required="required"
+          onChange={passwordChange}
+          id="password"
+          placeholder="Insira sua Senha"
+        />
+        <input
+          className="textos"
+          type="password"
+          id="confirm_password"
+          required="required"
+          onChange={confirmChange}
+          placeholder="Confirme a Senha"
+        />
+
+        {/* <input
+          className="textos"
+          type="password"
+          required="required"
+          onChange={passwordChange}
+          onFocus={() => capivaraOlhos(true)}
+          onBlur={() => capivaraOlhos(false)}
+          id="password"
+          placeholder="Insira sua Senha"
+        />
+        <input
+          className="textos"
+          type="password"
+          id="confirm_password"
+          required="required"
+          onChange={confirmChange}
+          onFocus={() => capivaraOlhos(true)}
+          onBlur={() => capivaraOlhos(false)}
+          placeholder="confirme a Senha"
+        /> */}
+
+        <button className="textos botaoRegistrar" type="submit" value="entar">
+          Registrar
+        </button>
+        {/* <div className="coletaneaBotoesLogar">
+          <button className="textos botaoRegistrar" type="submit" value="entar">
+            Registrar-se
+          </button>
+          <a
+            className="textos botaoRegistrar botaoUsuarUsuario"
+            onClick={() => primeiraVezAtualizar(true)}
+            href="#login"
+          >
+            Logar Usuário
+          </a>
+        </div> */}
+        {!primeiraVez &&
+          !sucesso &&
+          (message ? <div className="alertaRegistro">{message}</div> : "")}
+        {!primeiraVez &&
+          sucesso &&
+          (message ? (
+            <div className="alertaRegistroSucesso">{message}</div>
+          ) : (
+            ""
+          ))}
+      </form>
+    </>
+  );
 }
 
-export default RegisterComponent
+export default RegisterComponent;
--- front/goals-notes/src/pages/initial_page/initial_page.js
+++ front/goals-notes/src/pages/initial_page/initial_page.js
@@ -1,75 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import LoginComponent from '../../components/login.js';
-import RegisterComponent from '../../components/register.js';
-import { token_verify } from '../../services/api_requests.js';
-import '../../components/Style/loginStyle.css';
-import { remove_token } from '../../utils/token_verify.js';
+import React, { useEffect } from "react";
+import LoginComponent from "../../components/login.js";
+import RegisterComponent from "../../components/register.js";
+import "../../components/Style/loginStyle.css";
+import { token_verify } from "../../services/api_requests.js";
+import { remove_token } from "../../utils/token_verify.js";
 
 const Initial = () => {
-  const [telaMaiorCelular, setTelaMaiorCelular] = useState(window.innerWidth > 1500);
-
   async function verify(token) {
     try {
-      const response = await token_verify(token)
-      const id = response.id
-      if (sessionStorage.getItem("first_acess")){
+      const response = await token_verify(token);
+      const id = response.id;
+      if (sessionStorage.getItem("first_acess")) {
         window.location.href = `/CapCreate`;
       } else if (id) {
         window.location.href = `${id}/goals`;
       } else {
-        console.log('é necessário realizar o login');
+        console.log("é necessário realizar o login");
         remove_token();
       }
     } catch {
-      console.log('validation error');
+      console.log("validation error");
     }
   }
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       verify(token);
     }
   }, []);
 
-  useEffect(() => {
-    const verificarTamanhoDaTela = () => {
-      setTelaMaiorCelular(window.innerWidth > 1500);
-    };
-
-    window.addEventListener('resize', verificarTamanhoDaTela);
-
-    return () => {
-      window.removeEventListener('resize', verificarTamanhoDaTela);
-    };
-  }, []);
-
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
   return (
-    <section className='sectionLoginForm'>
-      {telaMaiorCelular && (
-        <>
-          <img className='logoCapivara capivaraOlha' src='/content/capivaraSemOlhos.png' alt="capivaraSemOlhos" />
-          <div id='cap' className="eyes">
-            <div className="eye" id="leftEye"></div>
-          </div>
-        </>
-      )}
-      <div id='login' className='cardLogin grid'>
-        <LoginComponent />
+    <section className="sectionLoginForm">
+      <img
+        className="logoCapivara capivaraOlha"
+        src="/content/capivaraSemOlhos.png"
+        alt="capivaraSemOlhos"
+      />
+      <div id="cap" className="eyes">
+        <div className="eye" id="leftEye" />
       </div>
-      <div id='registro' className='cardRegister grid'>
-        <RegisterComponent />
+      <div></div>
+      <div className="login-wrapper">
+        <div id="login" className="cardLogin grid">
+          <LoginComponent />
+        </div>
+        <div id="registro" className="cardRegister grid">
+          <RegisterComponent />
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default Initial;
