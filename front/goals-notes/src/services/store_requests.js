d5c3d75e92910cc08b93b15f1b60912f0f251018
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-08-16 20:07:53 -0300

    adicionar verificação de ambiente

--- front/goals-notes/src/services/store_requests.js
+++ front/goals-notes/src/services/store_requests.js
@@ -1,6 +1,10 @@
+import get_api_url from "../config";
+
+const apiUrl = get_api_url()
+
 export async function getSkins(){
   try{
-    let response = await fetch(`http://127.0.0.1:5000/skins`)
+    let response = await fetch(apiUrl+`skins`)
     let user = await response.json()
     return user
   } catch(e){
