be606933ff892c39ac9d85c47cbde67b5c8a0c18
Caio Milani <caiocmilani@gmail.com>
2024-08-17 20:08:20 -0300

    Atualizando navbar

--- front/goals-notes/src/components/navbar.js
+++ front/goals-notes/src/components/navbar.js
@@ -2,10 +2,13 @@ import React from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { useState } from "react";
 import { TiThMenu } from "react-icons/ti";
+import { useParams } from 'react-router-dom';
+import AmigoFotoComponent from '../components/amigoFoto.js';
 import "./Style/navbar.css";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+  const { id } = useParams();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -15,9 +18,7 @@ function Navbar() {
       <nav className="navbar navbar-light shadow navbar_view">
         <div className="container-fluid">
           <h1 className="menu_title" href="#">
-            <a href="#" className="menu_link">
-              Portifólio
-            </a>
+              Goal notes
           </h1>
           <TiThMenu className="burguer_menu" onClick={handleShow} />
         </div>
@@ -28,33 +29,44 @@ function Navbar() {
           className="offcanvas"
         >
           <Offcanvas.Header>
-            <h2 className="menu_title">Bem-vindo ao meu portifólio!</h2>
-            <TiThMenu className="burguer_menu" onClick={handleClose} />
+            <AmigoFotoComponent id = {id}/>
+            <h2 className="menu_title_hide">Vamos navegar juntos pelo site!</h2>
           </Offcanvas.Header>
-          <ul>
+          <ul className="navbar_items">
             <li className="menu_item_nav" onClick={handleClose}>
-              <a href="#" className="menu_link">
-                Início
+              <a className="menu_link center" href="home" >
+                <div className="item_home">
+                  <a className="menu_link btn_navbar_home">
+                    home
+                  </a>
+                </div>
               </a>
             </li>
             <li className="menu_item_nav" onClick={handleClose}>
-              <a href="#about" className="menu_link">
-                Sobre mim
+              <a className="menu_link center" href="goals" >
+                <div className="item_metas">
+                  <a className="menu_link btn_navbar_metas">
+                    metas
+                  </a>
+                </div>
               </a>
             </li>
             <li className="menu_item_nav" onClick={handleClose}>
-              <a href="#skills" className="menu_link">
-                Habilidades
+              <a className="menu_link center" href="store" >
+                <div className="item_loja">
+                  <a className="menu_link btn_navbar_loja">
+                    loja
+                  </a>
+                </div>
               </a>
             </li>
             <li className="menu_item_nav" onClick={handleClose}>
-              <a href="#projetos" className="menu_link">
-                Projetos
-              </a>
-            </li>
-            <li className="menu_item_nav" onClick={handleClose}>
-              <a href="#footer" className="menu_link">
-                Entrar em contato
+              <a className="menu_link center" href="store" >
+                <div className="item_deslogar">
+                  <a className="menu_link btn_navbar_deslogar">
+                  deslogar
+                  </a>
+                </div>
               </a>
             </li>
           </ul>
